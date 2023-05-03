c113858561ba69578746cecda6e498a2e289d5a0
Николай <Doonn3@gmail.com>
2023-05-03 18:43:39 +0300

    feat: add route main, welcome, 404

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,20 @@
+import Header from '../widgets/Header';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import Welcome from '../pages/Welcome/Welcome';
+import Main from '../pages/Main/Main';
+import PageNotFound from '../pages/PageNotFound/PageNotFound';
 
 function App() {
   return (
     <>
-      <div>Init</div>
+      <Header />
+      <Routes>
+        <Route path="" element={<Welcome />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </>
   );
 }
