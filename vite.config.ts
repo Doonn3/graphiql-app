2b79b084653c1c529251e777a38c5d796b470704
Николай <Doonn3@gmail.com>
2023-05-06 13:42:34 +0300

    feat: add svgr plugin and alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react(), eslint(), svgr()],
   resolve: {
     alias: {
-      '@assets': './src/shared/assets',
+      '@entities': path.resolve(__dirname, "src/entities"),
+      '@app': path.resolve(__dirname, "src/app"),
+      '@assets': path.resolve(__dirname, "src/shared/assets"),
+      '@shared': path.resolve(__dirname, "src/shared"),
+      '@features': path.resolve(__dirname, "src/features"),
+      '@pages': path.resolve(__dirname, "src/pages"),
+      '@widgets': path.resolve(__dirname, "src/widgets"),
     },
   },
 });
