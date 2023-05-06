c36f46e02f2ae5f72078f651ceb86db5a14c5cce
Николай <Doonn3@gmail.com>
2023-05-06 13:27:21 +0300

    refactor: ide widget

--- src/widgets/IDE/IDE.tsx
+++ src/widgets/IDE/IDE.tsx
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+import { VerticalResizePanel, HorizontalResizePanel, Layout } from '../../features/ResizePanel';
+import TextEditor from '../../features/TextEditor/TextEditor';
+import PanelTool from '../PanelTool/PanelTool';
+import ResponceView from '../../features/ResponceView/ResponceView';
 import style from './ide.module.scss';
 
 const defaultText = `
@@ -11,40 +15,65 @@ const defaultText = `
 
 interface IIDE {
   handler: (data: string) => void;
+  responce: string;
 }
 
 function IDE(props: IIDE) {
-  const [value, setValue] = useState(defaultText);
+  const [isVariablesActive, setVariablesActive] = useState(true);
+  const [isHeadersActive, setHeadersActive] = useState(false);
+
+  const [text, setText] = useState(defaultText);
 
   const handlerButtonClick = () => {
-    props.handler(value);
+    props.handler(text);
   };
 
-  const handlerKeyDown = (event: React.KeyboardEvent) => {
-    if (event.ctrlKey && event.code === 'Space') {
-      props.handler(value);
-    }
+  const handlerGetText = (data: string) => {
+    setText(data);
   };
 
-  const handlerInput = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setValue(event.currentTarget.value);
+  const handlerVariablesClick = () => {
+    setVariablesActive(true);
+    setHeadersActive(false);
+  };
+  const handlerHeadersClick = () => {
+    setHeadersActive(true);
+    setVariablesActive(false);
   };
 
   return (
-    <section className={style.ide}>
-      <div className={style.header}>
-        <button className={style.btn} onClick={handlerButtonClick}>
-          Start
-        </button>
-      </div>
-
-      <textarea
-        className={style.edit}
-        onKeyDown={handlerKeyDown}
-        onInput={handlerInput}
-        value={value}
-      ></textarea>
-    </section>
+    <>
+      <PanelTool />
+      <VerticalResizePanel>
+        <Layout>
+          <HorizontalResizePanel>
+            <Layout>
+              <section className={style.ide}>
+                <div className={style.header}>
+                  <button className={style.btn} onClick={handlerButtonClick}>
+                    Start
+                  </button>
+                </div>
+                <TextEditor defaultText={text} handler={handlerGetText} />
+              </section>
+            </Layout>
+            <Layout>
+              <section className={style.ide}>
+                <div className={style.header}>
+                  <button onClick={handlerVariablesClick}>Variables</button>
+                  <button onClick={handlerHeadersClick}>Headers</button>
+                </div>
+                {isVariablesActive && <TextEditor defaultText="VARIABLES" />}
+                {isHeadersActive && <TextEditor defaultText="HEADERS" />}
+              </section>
+            </Layout>
+          </HorizontalResizePanel>
+        </Layout>
+        <Layout>
+          <ResponceView data={props.responce} />
+        </Layout>
+      </VerticalResizePanel>
+    </>
   );
 }
 
