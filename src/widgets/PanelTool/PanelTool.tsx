7eaa13e1c4347e62852895053e3c07caa9eb1500
Николай <Doonn3@gmail.com>
2023-05-11 22:10:52 +0300

    refactor: PanelTool

--- src/widgets/PanelTool/PanelTool.tsx
+++ src/widgets/PanelTool/PanelTool.tsx
@@ -1,13 +1,40 @@
 import { ReactComponent as DocumentationIcon } from '@assets/documentation.svg';
+import { useState } from 'react';
 
 import style from './paneltool.module.scss';
 
 function PanelTool() {
+  const [isActiveDoc, setActiveDoc] = useState(false);
+
+  const handlerDocClick = () => {
+    setActiveDoc(!isActiveDoc);
+  };
+
   return (
-    <div className={style.panel}>
-      <div className={style.doc}>
-        <DocumentationIcon />
+    <div className={`${style.panel} ${isActiveDoc ? style.sliding : ''}`}>
+      <div className={style.panel__block}>
+        <div className={style.panel__doc}>
+          <DocumentationIcon onClick={handlerDocClick} />
+        </div>
       </div>
+
+      {isActiveDoc && (
+        <div className={style.doc}>
+          <div className={style.doc__header}>
+            <h3 className={style.doc__title}>Doc</h3>
+            <input className={style.doc__search} type="text" />
+          </div>
+          <div className={style.doc__info}>
+            <b>Test</b>
+            <b>Test</b>
+            <b>Test</b>
+            <b>Test</b>
+            <b>Test</b>
+            <b>Test</b>
+            <b>Test</b>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
