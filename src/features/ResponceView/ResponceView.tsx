4096af8f428af300afd6998267968f85da2d21c3
Николай <Doonn3@gmail.com>
2023-05-11 22:10:21 +0300

    refactor: ResponceView

--- src/features/ResponceView/ResponceView.tsx
+++ src/features/ResponceView/ResponceView.tsx
@@ -1,24 +1,30 @@
-import LexerJson from '../../shared/LOGIC_EXPEREMENTAL/LexerJson';
-import style from './responce.module.scss';
-
-const le = new LexerJson();
-const jsonTest = `{
-    "data": {
-    "countries": [
-      {
-        "code": "AD"
-      },
-        ]
-    }
-}`;
+import './responce.scss';
+import { useCallback, useEffect, useRef } from 'react';
+import { createEditor, EditorViewType } from '@shared/Editor';
 
 interface IResponceView {
   data: string;
 }
 
 function ResponceView(props: IResponceView) {
-  console.log(le.parse(jsonTest));
-  return <div className={style.responce}>{props.data}</div>;
+  const editorRef = useRef<EditorViewType | null>(null);
+
+  const ref = useCallback(async (node: HTMLDivElement) => {
+    if (node && !editorRef.current) {
+      const { view } = createEditor(node, 'json', false);
+      editorRef.current = view;
+    }
+  }, []);
+
+  useEffect(() => {
+    if (editorRef.current) {
+      editorRef.current.dispatch({
+        changes: { from: 0, to: editorRef.current.state.doc.length, insert: props.data },
+      });
+    }
+  }, [props.data]);
+
+  return <div className="responce" ref={ref}></div>;
 }
 
 export default ResponceView;
