6450d8d0a3dcb061ded94360de8e987dc4ff4c23
Николай <Doonn3@gmail.com>
2023-05-28 18:40:10 +0300

    refactor: type doc

--- src/features/DocView/components/TypeDoc/TypeDoc.tsx
+++ src/features/DocView/components/TypeDoc/TypeDoc.tsx
@@ -1,23 +1,20 @@
-import { GraphQLFieldMap, GraphQLObjectType } from 'graphql';
-import { useState } from 'react';
-import GenerationContent, { GenContentType } from '../GenerationContent/GenerationContent';
+import { GraphQLObjectType } from 'graphql';
+import GenerationContent from '../GenerationContent/GenerationContent';
 
 interface TypeDocProps {
   type: GraphQLObjectType;
 }
 
 function TypeDoc({ type }: TypeDocProps) {
-  const fields = type.getFields();
-  const [genContent, setGenContent] = useState<GenContentType>();
+  // const fields = type.getFields();
+  // const [genContent, setGenContent] = useState<GenContentType>();
 
-  console.log(type);
-
-  const handlerClick = (type: GraphQLObjectType) => {
-    setGenContent({
-      typeObjectQuery: type,
-      fields: type.getFields(),
-    });
-  };
+  // const handlerClick = (type: GraphQLObjectType) => {
+  //   setGenContent({
+  //     typeObjectQuery: type,
+  //     fields: type.getFields(),
+  //   });
+  // };
 
   return (
     <div>
