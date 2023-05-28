bb53d073a77aab0907481b63dd02ecc964c03ffb
Николай <Doonn3@gmail.com>
2023-05-28 06:49:52 +0300

    refactor: fetch api

--- src/shared/FetchApi/FetchApi.ts
+++ src/shared/FetchApi/FetchApi.ts
@@ -1,28 +1,42 @@
 import { buildClientSchema, getIntrospectionQuery, GraphQLSchema, printSchema } from 'graphql';
 
+interface ErrorFieldsType {
+  message?: string;
+  stack?: string;
+}
+
+interface ErrorType {
+  error?: ErrorFieldsType;
+}
+
 class FetchApi {
   static instance: FetchApi = new FetchApi();
   private readonly url = 'https://countries.trevorblades.com';
 
-  public async RequestQuery(queryText: string, variable: string): Promise<object> {
-    let variableValue = {};
-    if (variable) {
-      variableValue = JSON.parse(variable);
+  public async RequestQuery(queryText: string, variable: string): Promise<object | null> {
+    try {
+      let variableValue = {};
+      if (variable) {
+        variableValue = JSON.parse(variable);
+      }
+      const res = await fetch(this.url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          query: queryText,
+          variables: variableValue,
+        }),
+      });
+
+      const data: object = await res.json();
+
+      return data;
+    } catch (error) {
+      console.log(error);
+      return null;
     }
-    const res = await fetch(this.url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        query: queryText,
-        variables: variableValue,
-      }),
-    });
-
-    const data: object = await res.json();
-
-    return data;
   }
 
   public async RequestIntrospection(): Promise<GraphQLSchema | null> {
