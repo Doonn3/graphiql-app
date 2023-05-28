f146191b13384c671efb777b68b628647f4bf395
Николай <Doonn3@gmail.com>
2023-05-28 18:38:00 +0300

    refactor: delete deprecated

--- src/features/DocView/TEST/test.ts
+++
@@ -1,8 +0,0 @@
-import FetchApi from '../../../shared/FetchApi/FetchApi';
-
-async function getSchema() {
-  const testGetSchema = await FetchApi.instance.RequestIntrospection();
-  return testGetSchema;
-}
-
-export { getSchema };
