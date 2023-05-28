8345cc5446d485a0e6df7e5366c06829730cd464
Николай <Doonn3@gmail.com>
2023-05-28 22:59:31 +0300

    refactor: delete console.log and comments

--- src/features/DocView/components/TypeDoc/TypeDoc.tsx
+++ src/features/DocView/components/TypeDoc/TypeDoc.tsx
@@ -6,22 +6,9 @@ interface TypeDocProps {
 }
 
 function TypeDoc({ type }: TypeDocProps) {
-  // const fields = type.getFields();
-  // const [genContent, setGenContent] = useState<GenContentType>();
-
-  // const handlerClick = (type: GraphQLObjectType) => {
-  //   setGenContent({
-  //     typeObjectQuery: type,
-  //     fields: type.getFields(),
-  //   });
-  // };
-
   return (
     <div>
-      {/* <span>Root Types</span> */}
       <div>
-        {/* <span>query:</span> */}
-        {/* <span onClick={() => handlerClick(type)}>{type.name}</span> */}
         <GenerationContent typeObjectQuery={type} />
       </div>
     </div>
