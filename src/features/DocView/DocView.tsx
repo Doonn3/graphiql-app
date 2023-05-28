6aeeb50345ff8a6b1d2c1637d0ff01e268b2773d
Николай <Doonn3@gmail.com>
2023-05-28 22:49:17 +0300

    refactor: doc view

--- src/features/DocView/DocView.tsx
+++ src/features/DocView/DocView.tsx
@@ -1,4 +1,5 @@
 import { GraphQLSchema } from 'graphql';
+import React, { useState } from 'react';
 import SchemaDoc from './components/SchemaDoc/SchemaDoc';
 
 // import Search from './components/Search/Search';
@@ -6,12 +7,19 @@ import style from './doc-view.module.scss';
 import './helpers/style/doc.style.scss';
 
 // import { getSchema } from './TEST/test';
+interface ContextType {
+  isClick: boolean;
+  handlerClick?: () => void;
+}
+
+export const DocViewContext = React.createContext<ContextType>({ isClick: false });
 
 interface PropsDocViewType {
   schema?: GraphQLSchema;
 }
 
 function DocView(props: PropsDocViewType) {
+  const [isClickBack, setClickBack] = useState(false);
   // const [schema, setSchema] = useState<GraphQLSchema | undefined>(props.schema);
   // const dispatch = useDispatch();
 
@@ -38,16 +46,25 @@ function DocView(props: PropsDocViewType) {
   //   fields();
   // }, []);
 
+  const handlerBackDocClick = () => {
+    setClickBack(!isClickBack);
+  };
+
   return (
-    <div className={style.doc}>
-      <div className={style.doc__header}>
-        <h3 className={style.doc__title}>Doc</h3>
-        {/* <Search handler={handlerSearch} /> */}
+    <DocViewContext.Provider value={{ isClick: isClickBack }}>
+      <div className={style.doc}>
+        <div className={style.doc__header}>
+          <h3 className={style.doc__title}>Doc</h3>
+          <span className={style.doc__back} onClick={handlerBackDocClick}>
+            B
+          </span>
+          {/* <Search handler={handlerSearch} /> */}
+        </div>
+        <div className={style.doc__info}>
+          <SchemaDoc schema={props.schema} />
+        </div>
       </div>
-      <div className={style.doc__info}>
-        <SchemaDoc schema={props.schema} />
-      </div>
-    </div>
+    </DocViewContext.Provider>
   );
 }
 
